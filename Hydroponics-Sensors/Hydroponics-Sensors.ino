c60ff6dfb73bf2b617a461385534e12604351b19
Johnathan Pruitt <pruittj2000@Yahoo.com>
2023-11-10 13:38:45 -0600

    Scanner to Identify I2C Components

--- Hydroponics-Sensors/Hydroponics-Sensors.ino
+++ Hydroponics-Sensors/Hydroponics-Sensors.ino
@@ -5,26 +5,44 @@
 #define Environment Environment_I2C
 
 void setup() {
-
   Wire.begin();
-  pinMode(LED,OUTPUT);    //Sets the pinMode to Output 
+  // Environment.begin();
 
-  Serial.begin(9600);
-  Environment.begin();
+  Oled.begin();
+  Oled.setFlipMode(true); // Sets the rotation of the screen
+
+  
+  // Serial.begin(9600);
+  
+
+  
+
+  // pinMode(LED,OUTPUT);    //Sets the pinMode to Output 
 }
  
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH); //Sets the voltage to high 
-  delay(1000);					   //Waits for 1000 milliseconds 
-  digitalWrite(LED, LOW);  //Sets the voltage to low
-  delay(1000);             //Waits for 1000 milliseconds 
+  // digitalWrite(LED, HIGH); //Sets the voltage to high 
+  // delay(1000);					   //Waits for 1000 milliseconds 
+  // digitalWrite(LED, LOW);  //Sets the voltage to low
+  // delay(1000);             //Waits for 1000 milliseconds 
 
-  Serial.print("Temperature = ");
-  Serial.print(Environment.readTemperature()); //print temperature
-  Serial.println(" C");
-  Serial.print("Humidity = ");
-  Serial.print(Environment.readHumidity()); //print humidity
-  Serial.println(" %");
-  delay(2000);
+  // Serial.print("Temperature = ");
+  // Serial.print(Environment.readTemperature());
+  // Serial.println(" C");
+  
+  // Serial.print("Humidity = ");
+  // Serial.print(Environment.readHumidity());
+  // Serial.println(" %");
+
+  int random_value = analogRead(A0);
+ 
+  Oled.setFont(u8x8_font_chroma48medium8_r);
+  Oled.setCursor(0, 33);
+  Oled.print("Temp");
+  // Oled.print(Environment.readTemperature());
+  Oled.print(" CDS");
+  Oled.refreshDisplay();
+  
+  delay(1000); // Delay for OLED display update
 }
