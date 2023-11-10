d05321c147d3a5b35520461a4359fe8dc98c295c
Johnathan Pruitt <pruittj2000@Yahoo.com>
2023-11-10 12:36:59 -0600

    Added Temperature & Humidity Sensor Readings

--- Hydroponics-Sensors/Hydroponics-Sensors.ino
+++ Hydroponics-Sensors/Hydroponics-Sensors.ino
@@ -1,8 +1,16 @@
-  #define LED 6
- 
+  	
+#include "Arduino_SensorKit.h"
+  
+#define LED 6
+#define Environment Environment_I2C
+
 void setup() {
-  // put your setup code here, to run once:
+
+  Wire.begin();
   pinMode(LED,OUTPUT);    //Sets the pinMode to Output 
+
+  Serial.begin(9600);
+  Environment.begin();
 }
  
 void loop() {
@@ -11,4 +19,12 @@ void loop() {
   delay(1000);					   //Waits for 1000 milliseconds 
   digitalWrite(LED, LOW);  //Sets the voltage to low
   delay(1000);             //Waits for 1000 milliseconds 
+
+  Serial.print("Temperature = ");
+  Serial.print(Environment.readTemperature()); //print temperature
+  Serial.println(" C");
+  Serial.print("Humidity = ");
+  Serial.print(Environment.readHumidity()); //print humidity
+  Serial.println(" %");
+  delay(2000);
 }
